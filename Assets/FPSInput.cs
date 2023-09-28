ca620d8e0b0bd99135a3fe96b9a5386bfd31eb23
Andrian0v <112649696+Andrian0v@users.noreply.github.com>
2023-09-28 15:34:33 +0300

    1
    1

---
+++ Assets/FPSInput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Controll Script/FPS Input")]
+public class FPSInput : MonoBehaviour {
+
+	private CharacterController _charController;
+
+	public float gravity = -9.8f;
+
+	void Start () {
+		_charController = GetComponent<CharacterController> ();
+	}
+
+	public float speed = 6.0f;
+	void Update () {
+		float deltaX = Input.GetAxis ("Horizontal") * speed;
+		float deltaZ = Input.GetAxis ("Vertical") * speed;
+		Vector3 movement = new Vector3 (deltaX, 0, deltaZ);
+		movement = Vector3.ClampMagnitude (movement, speed);
+		movement.y = gravity;
+
+		movement *= Time.deltaTime;
+		movement = transform.TransformDirection (movement);
+		_charController.Move (movement);
+	}
+}
---
+++ Assets/NetworkService.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System;
+using UnityEngine.Experimental.Networking;
+
+
+public class NetworkService 
+{
+	private const string xmlApi = "http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&mode=xml&APPID=188e2e8cb4ba801748304cf287a4ca46";
+	private const string jsonApi = "http://api.openweathermap.org/data/2.5/weather?q=Moscow,ru&APPID=188e2e8cb4ba801748304cf287a4ca46";
+	private IEnumerator CallAPI(string url, Action<string> callback) 
+	{
+		using (UnityWebRequest request = UnityWebRequest.Get(url)) 
+		{
+			yield return request.Send(); 
+			if (request.responseCode != (long)System.Net.HttpStatusCode.OK) 
+			{
+				Debug.LogError("response error: " + request.responseCode);
+			} 
+			else 
+			{
+				callback(request.downloadHandler.text);
+			}
+		}
+	}
+	public IEnumerator GetWeatherJSON(Action<string> callback) 
+	{
+		return CallAPI(jsonApi, callback);
+	}
+	public IEnumerator GetWeatherXML(Action<string> callback) 
+	{
+		return CallAPI(xmlApi, callback);
+	}
+}
